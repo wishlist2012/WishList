66424512bfd4cfcb0e8e431ff8ad2ba388e0a38e
Vytalyi Denysenko <vytalyi.denysenko@gmail.com>
2012-10-05 23:19:23 +0300

    WhishList repository project initial

---
+++ WhishList_Repository/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WhishList_Repository")]
+[assembly: AssemblyDescription("WhishList Repository Assembly")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhishList_Repository")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71ea9c62-6776-4421-bd67-f1d3e42654e6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
