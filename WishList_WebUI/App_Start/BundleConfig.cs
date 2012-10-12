fce69d75582d99dd70b496900259c608547a87d7
AlexlL <levenetsalexey@gmail.com>
2012-10-12 18:18:07 +0300

    added HomeCobtroler and view, Style files, jquery

--- WishList_WebUI/App_Start/BundleConfig.cs
+++ WishList_WebUI/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace WishList_WebUI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/site.css",
+                        "~/Content/LayoutStyle.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
